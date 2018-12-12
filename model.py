24885e47fa3d6f626c71c1a71375ace1548e9d1c
Ahmad <ahmad.khajehnejad@gmail.com>
2018-12-13 01:42:34 +0330

    Completed. But still needs comments for documentations.

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pickle
-
+import scipy.stats
 
 def _make_tree_structure(n, num_parents):
     adjacency_matrix = np.zeros([n,n])
     parents = [None] * n
-    parents[0] = np.array([])
+    parents[0] = np.array([], dtype=np.int)
     for i in range(1,n):
-        parents[i] = np.random.choice(list(range(i)),np.minimum(num_parents, i),replace=False)
+        parents[i] = np.array(np.random.choice(list(range(i)),np.minimum(num_parents, i), replace=False), dtype=np.int)
         adjacency_matrix[i,parents[i]] = 1
         adjacency_matrix[parents[i],i] = 1
         for j in parents[i]:
@@ -32,8 +32,6 @@ def _make_Bayesian_tree_parameters(parents):
             theta_select.append(w)
     return theta_select
 
-
-
 class SyntheticStructuredDataGenerator:
     
     
@@ -41,8 +39,8 @@ class SyntheticStructuredDataGenerator:
         outcome = np.zeros(self.d)
         outcome[0] = np.random.sample()
         for i in range(1,self.d):
-            ind = np.where(np.random.multinomial(1, self.theta_select[i]) > 0)[0]
-            mu = outcome[self.parents[i][ind]]
+            ind = np.where(np.random.multinomial(1, self.theta_inner[i]) > 0)[0][0]
+            mu = outcome[self.parents_inner[i][ind]]
             outcome[i] = np.random.sample() * self.sigma_inner + mu
         return outcome
     
@@ -51,7 +49,7 @@ class SyntheticStructuredDataGenerator:
         for sn in range(num_samples):
             all_outcomes[sn, :self.d] = self._generate_root_sample()
             for i in range(1,self.D):
-                ind = np.where(np.random.multinomial(1, self.theta_outer[i]) > 0)[0]
+                ind = np.where(np.random.multinomial(1, self.theta_outer[i]) > 0)[0][0]
                 ind_mu = self.parents_outer[i][ind]
                 mu = all_outcomes[sn, (ind_mu * self.d) : ((ind_mu+1) * self.d)]
                 all_outcomes[sn, (i*self.d):((i+1)*self.d)] = np.random.sample(self.d) * self.sigma_outer + mu
@@ -59,10 +57,10 @@ class SyntheticStructuredDataGenerator:
     
 
     def __init__(self, D=20, d=30, sigma_outer=0.2, sigma_inner=0.2, num_parents_outer=3, num_parents_inner=5):
+
         self.D, self.d = D, d
         self.sigma_outer, self.sigma_inner = sigma_outer, sigma_inner
         self.num_parents_outer, self.num_parents_inner = num_parents_outer, num_parents_inner
-
         self.adjacency_matrix_inner, self.parents_inner = _make_tree_structure(d, num_parents_inner)
         self.theta_inner = _make_Bayesian_tree_parameters(self.parents_inner)
         self.adjacency_matrix_outer, self.parents_outer = _make_tree_structure(D, num_parents_outer)
@@ -86,6 +84,41 @@ class SyntheticStructuredDataGenerator:
                          self.parents_outer, self.parents_inner,\
                          self.theta_outer, self.theta_inner] = pickle.load(fin)
 
-    def log_likelihood(X):
-        raise('not implemented yet!')
+
+    def _root_log_likelihood(self, x):
+        std_normal = scipy.stats.norm()
+
+        if len(x.shape) == 1:
+            x = x.reshape([1,-1])
+
+        n = x.shape[0]
+
+        ll = np.zeros([n])
+
+        for i in range(1, self.d):
+            likelihood = np.zeros([n])
+            parents = x[:, self.parents_inner[i]]
+            for j in range(parents.shape[1]):
+                likelihood += std_normal.pdf((x[:, i] - parents[:, j]) / self.sigma_inner) * self.theta_inner[i][j]
+            ll += np.log(likelihood)
+        return ll
+
+
+    def log_likelihood(self, x):
+        std_multivariate_normal = scipy.stats.multivariate_normal(np.zeros([self.d]), np.eye(self.d))
+
+        if len(x.shape) == 1:
+            x = x.reshape([1, -1])
+        n = x.shape[0]
+
+        ll = self._root_log_likelihood(x[:,:self.d])
+        for i in range(1, self.D):
+            likelihood = np.zeros(n)
+            x_i = x[:, (i * self.d) : ((i + 1) * self.d) ]
+            parents_ind = self.parents_outer[i]
+            for j in range(len(parents_ind)):
+                x_par_j = x[:, (parents_ind[j]*self.d) : ((parents_ind[j]+1)*self.d) ]
+                likelihood += std_multivariate_normal.pdf((x_i - x_par_j) / self.sigma_outer) * self.theta_outer[i][j]
+            ll += np.log(likelihood)
+        return ll
 
